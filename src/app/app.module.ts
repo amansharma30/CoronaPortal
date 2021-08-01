8f79ca1b03a67b68d344993bfcd77706bc6d5644
amansharma <aman8803468878@gmail.com>
2021-08-01 17:15:52 +0200

    Inital Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { DataViewComponent } from './data-view/data-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material-module.ts
@@ -0,0 +1,10 @@
+import {MatTableModule} from '@angular/material/table';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+    exports: [
+        MatTableModule
+    ]
+  })
+
+  export class MaterialModule {}
